6624893f64d19b1c783316b1eb2b86fae8efe38c
lixinwei <xinwei.li@winsuntech.cn>
2019-04-30 17:21:04 +0800

    fix bug in BackupNotification

--- src/file/BackupNotification.jsx
+++ src/file/BackupNotification.jsx
@@ -73,14 +73,14 @@ class BackupNotification extends React.PureComponent {
     console.log('render', this.state, this.props)
     return (
       <div>
-        { this.renderButton() }
+        {this.renderButton()}
         <DialogOverlay open={!!this.state.open}>
           {
             !!this.state.open && (
               <div style={{ width: 640, overflow: 'hidden' }} >
                 <div style={{ height: 64, display: 'flex', alignItems: 'center' }} >
                   <div style={{ fontSize: 16, marginLeft: 24 }}>
-                    { i18n.__('BackupNotification Dialog Title %s', list.length) }
+                    {i18n.__('BackupNotification Dialog Title %s', list.length)}
                   </div>
                   <div style={{ flexGrow: 1 }} />
                   <LIButton
@@ -95,69 +95,80 @@ class BackupNotification extends React.PureComponent {
                 <div style={{
                   height: 40, color: 'rgba(0,0,0,.54)', fontSize: 12, display: 'flex', alignItems: 'center'
                 }}>
-                  <div style={{ marginLeft: 24 }}> { i18n.__('Name') } </div>
+                  <div style={{ marginLeft: 24 }}> {i18n.__('Name')} </div>
                   <div style={{ width: 360 }} />
-                  <div> { i18n.__('Reason') } </div>
+                  <div> {i18n.__('Reason')} </div>
                 </div>
 
                 <SimpleScrollBar width={640} height={height} >
                   {
-                    list.map(l => (
-                      <div key={l.entry || l.files[0].entry} style={{ height: 56, width: '100%', display: 'flex', alignItems: 'center' }} >
-                        <div style={{ width: 24, marginLeft: 24 }} className="flexCenter" >
-                          <FolderIcon style={{ color: '#f9a825', width: 24, height: 24 }} />
-                        </div>
-                        <div style={{ width: 16 }} />
-
-                        <div style={{ width: 344, height: 56 }}>
-                          <div style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            margin: l.entry ? '12px 0 4px 0' : '18px 0 0 0'
-                          }}>
-                            {/* entry name */}
-                            <div
-                              style={{
-                                marginRight: 4,
-                                color: 'rgba(0,0,0,.76)',
-                                maxWidth: l.files && l.files.length > 1 ? 200 : 320
-                              }}
-                              className="text"
-                            >
-                              { l.name || (Array.isArray(l.files) && l.files[0] && l.files[0].name) }
-                            </div>
-
-                            <div>
-                              { l.files && l.files.length > 1 && i18n.__('And Other %s Items', l.files.length)}
-                            </div>
-                          </div>
-                          {/* local path */}
-                          <div style={{ color: 'rgba(0,0,0,.54)', fontSize: 12 }} className="text">
-                            { l.entry }
-                          </div>
-                        </div>
+                    list.map((l, i) => {
+                      const count = l.entry ? 1 : l.files ? l.files.length : l.entries ? l.entries.length : 1
+                      const name = l.name || (Array.isArray(l.files) && l.files[0] && l.files[0].name) ||
+                        (Array.isArray(l.entries) && l.entries[0] && l.entries[0].name) || ''
+                      return (
                         <div
-                          style={{
-                            width: 136,
-                            height: 56,
-                            color: 'rgba(0,0,0,.54)',
-                            fontSize: 12,
-                            display: 'flex',
-                            alignItems: 'center'
-                          }}
+                          key={name || i.toString()}
+                          style={{ height: 56, width: '100%', display: 'flex', alignItems: 'center' }}
                         >
-                          { !!l.error && convert(l.error.code) }
+                          {/* icon */}
+                          <div style={{ width: 24, marginLeft: 24 }} className="flexCenter" >
+                            <FolderIcon style={{ color: '#f9a825', width: 24, height: 24 }} />
+                          </div>
+                          <div style={{ width: 16 }} />
+
+                          <div style={{ width: 344, height: 56 }}>
+                            <div style={{
+                              display: 'flex',
+                              alignItems: 'center',
+                              margin: l.entry ? '12px 0 4px 0' : '18px 0 0 0'
+                            }}>
+                              {/* entry name */}
+                              <div
+                                style={{
+                                  marginRight: 4,
+                                  color: 'rgba(0,0,0,.76)',
+                                  maxWidth: count > 1 ? 200 : 320
+                                }}
+                                className="text"
+                              >
+                                {name}
+                              </div>
+
+                              <div>
+                                {count > 1 && i18n.__('And Other %s Items', count)}
+                              </div>
+                            </div>
+                            {/* local path */}
+                            <div style={{ color: 'rgba(0,0,0,.54)', fontSize: 12 }} className="text">
+                              {l.entry}
+                            </div>
+                          </div>
+                          {/* error code */}
+                          <div
+                            style={{
+                              width: 136,
+                              height: 56,
+                              color: 'rgba(0,0,0,.54)',
+                              fontSize: 12,
+                              display: 'flex',
+                              alignItems: 'center'
+                            }}
+                          >
+                            {!!l.error && convert(l.error.code)}
+                          </div>
+                          {/* actions */}
+                          {
+                            !!l.isWarning &&
+                            <FlatButton
+                              label={i18n.__('Force Backup')}
+                              primary
+                              onClick={() => this.forceBackup(l)}
+                            />
+                          }
                         </div>
-                        {
-                          l.isWarning &&
-                          <FlatButton
-                            label={i18n.__('Force Backup')}
-                            primary
-                            onClick={() => this.forceBackup(l)}
-                          />
-                        }
-                      </div>
-                    ))
+                      )
+                    })
                   }
                 </SimpleScrollBar>
                 <div style={{ height: 24 }} />
