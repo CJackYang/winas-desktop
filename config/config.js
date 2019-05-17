743fcea45e4d50592f3566e749a5ed946f307726
lixinwei <xinwei.li@winsuntech.cn>
2019-05-17 16:11:11 +0800

    change cloudAddress to aidingnan

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 const config = {
-  // cloudAddress: 'https://test.nodetribe.com/c/v1'
-  cloudAddress: 'http://abel-lb-test-2102038915.cn-north-1.elb.amazonaws.com.cn:12345/c/v1'
+  cloudAddress: 'https://test.aidingnan.com/c/v1'
 }
 
 module.exports = config
